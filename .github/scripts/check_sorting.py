27d2b7156c82b8058751ccd35d4d8f6f8d5a8bf5
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-08-30 15:42:32 +1000

    rewrite of sorting checks to be more robust

--- .github/scripts/check_sorting.py
+++ .github/scripts/check_sorting.py
@@ -1,6 +1,10 @@
 import re
 
 
+def clean_item(item):
+    return re.sub(r'\[|\]|\(|\)|`', '', item.split('\n')[0].lower())
+
+
 def check_alphabetical_order(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
@@ -13,34 +17,19 @@ def check_alphabetical_order(file_path):
     for section in sections:
         lines = section.strip().split('\n')
         section_name = lines[0].strip()
-        items = []
-        current_subsection = None
-        for line in lines[1:]:
-            if line.strip().startswith('### '):
-                if items:
-                    sorted_items = sorted(items, key=lambda x: re.sub(
-                        r'\[|\]|\(|\)|`', '', x.lower()))
-                    if items != sorted_items:
-                        all_sorted = False
-                        change = f"In section '{section_name}'"
-                        if current_subsection:
-                            change += f", subsection '{current_subsection}'"
-                        change += ":\n" + "\n".join(sorted_items)
-                        changes.append(change)
-                current_subsection = line.strip()[4:]
-                items = []
-            elif line.strip().startswith('- ['):
-                items.append(line.strip())
+        subsections = re.split(r'^###\s', section, flags=re.MULTILINE)[1:]
 
-        if items:
-            sorted_items = sorted(items, key=lambda x: re.sub(
-                r'\[|\]|\(|\)|`', '', x.lower()))
+        for subsection in subsections:
+            sublines = subsection.strip().split('\n')
+            subsection_name = sublines[0].strip()
+            items = [line.strip() for line in sublines[1:]
+                     if line.strip().startswith('- [')]
+
+            sorted_items = sorted(items, key=clean_item)
             if items != sorted_items:
                 all_sorted = False
-                change = f"In section '{section_name}'"
-                if current_subsection:
-                    change += f", subsection '{current_subsection}'"
-                change += ":\n" + "\n".join(sorted_items)
+                change = f"In section '{section_name}', subsection '{subsection_name}':\n"
+                change += "\n".join(sorted_items)
                 changes.append(change)
 
     return all_sorted, changes
